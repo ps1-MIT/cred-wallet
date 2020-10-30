48eda422c4a3375de36e15212471b5bd0efc2969
Reykjavik151 <Reykjavik151@gmail.com>
2020-10-30 16:39:59 +0200

    refactor: change android app name

--- android/app/src/main/java/com/pickles/auctions/MainActivity.java
+++ android/app/src/main/java/com/pickles/auctions/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pickles.auctions;
+package com.credwallet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Pickles";
+    return "CredWallet";
   }
 }
--- android/app/src/main/java/com/pickles/auctions/MainApplication.java
+++ android/app/src/main/java/com/pickles/auctions/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pickles.auctions;
+package com.credwallet;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.pickles.auctions.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.credwallet.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
