662f316c0e930f835576832d5d031692a9277bde
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-03 12:38:45 +0200

    feat: added test navigation button to PinScreen

--- app/screens/Pin/index.tsx
+++ app/screens/Pin/index.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent, useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { FunctionComponent } from 'react';
+import { SafeAreaView, TouchableOpacity } from 'react-native';
 import { Text } from '../../components';
 import { PinScreenProps } from './pin.props';
 import { styles } from './pin.styles';
@@ -7,14 +7,12 @@ import { styles } from './pin.styles';
 export const PinScreen: FunctionComponent<PinScreenProps> = ({
   navigation,
 }) => {
-  useEffect(() => {
-    // TODO: Timeout for loading simulation. Replace it with data fetching
-    setTimeout(() => navigation.replace('MainTabs'), 500);
-  }, [navigation]);
-
   return (
     <SafeAreaView style={styles.root}>
       <Text preset="title">There will be pin...</Text>
+      <TouchableOpacity onPress={() => navigation.replace('MainTabs')}>
+        <Text>Verify!</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
