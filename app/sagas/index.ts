c55d89e97608b13c1b59940b3bb20bf455a3bb90
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-03 12:35:21 +0200

    refactor: changed sagas structure

--- app/sagas/index.ts
+++ app/sagas/index.ts
@@ -2,11 +2,13 @@ import { all, takeLatest } from 'redux-saga/effects';
 import { GetUserAccountAction, userActionTypes } from '../redux/user';
 
 import { getUser } from './user';
-import { appStateListenerSaga } from './appState';
+import { appStateListenerSaga } from './event-channel';
 
 export default function* rootSaga() {
   yield all([
+    // Event Channels
     appStateListenerSaga(),
+
     takeLatest<GetUserAccountAction>(userActionTypes.GET_USER, getUser),
   ]);
 }
