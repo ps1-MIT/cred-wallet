9d14fa651fc2e45ac3accca50b8282ce42a52490
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-04 11:52:03 +0200

    feat: inited pin code component

---
+++ app/components/PinCode/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+import { PinCodeProps } from './pin-code.props';
+import { styles } from './pin-code.styles';
+
+export const PinCode: React.FunctionComponent<PinCodeProps> = () => {
+  return <View style={styles.container}></View>;
+};
---
+++ app/components/PinCode/pin-code.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet, ViewStyle } from 'react-native';
+
+interface PinCodeStyles {
+  container: ViewStyle;
+}
+
+export const styles = StyleSheet.create<PinCodeStyles>({
+  container: {
+    flex: 1,
+  },
+});
