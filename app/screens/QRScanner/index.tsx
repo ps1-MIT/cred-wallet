acdb7c5dffd8643030c5e17029190840c4c7b9eb
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-09 18:28:01 +0200

    feat: added QRScannerScreen qr scanning alpha version

--- app/screens/QRScanner/index.tsx
+++ app/screens/QRScanner/index.tsx
@@ -1,5 +1,7 @@
-import React, { FunctionComponent, useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { FunctionComponent, useCallback, useEffect } from 'react';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+// import { RNCamera } from 'react-native-camera';
 
 import { QRScannerScreenProps } from './qr-scanner.props';
 import { styles } from './qr-scanner.styles';
@@ -8,9 +10,27 @@ export const QRScannerScreen: FunctionComponent<QRScannerScreenProps> = ({
   navigation,
   // route,
 }) => {
-  useEffect(() => {
-    setTimeout(() => navigation.goBack(), 2000);
+  const onSuccess = useCallback((e) => {
+    console.tron.log(e.data);
   }, []);
 
-  return <SafeAreaView style={styles.root}></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.root}>
+      <QRCodeScanner
+        onRead={onSuccess}
+        // flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text>
+            Go to <Text>wikipedia.org/wiki/QR_code</Text> on your computer and
+            scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity>
+            <Text>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    </SafeAreaView>
+  );
 };
