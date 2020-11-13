9967ad0df30739f021ede9d80b315c2063fe1329
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-13 12:52:45 +0200

    refactor: moved biometry options to options file

--- app/components/VerifyPanel/index.tsx
+++ app/components/VerifyPanel/index.tsx
@@ -5,6 +5,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import _ from 'lodash';
 
 import {
+  BIOMETRY_OPTIONS,
   CIPHERS_BY_ROWS,
   PANEL_STATUS,
   PIN_LENGTH,
@@ -171,19 +172,7 @@ export const VerifyPanel: React.FunctionComponent<VerifyPanelProps> = ({
   }, [enteredPin, keychainPin, setPanelStatus]);
 
   const launchBiometricVerify = useCallback(() => {
-    const optionalConfigObject = {
-      title: 'Authentication Required', // Android
-      imageColor: '#e00606', // Android
-      imageErrorColor: '#ff0000', // Android
-      sensorDescription: 'Touch sensor', // Android
-      sensorErrorDescription: 'Failed', // Android
-      cancelText: 'Cancel', // Android
-      fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
-      unifiedErrors: false, // use unified error messages (default false)
-      passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
-    };
-
-    TouchID.authenticate('To access your accounts', optionalConfigObject)
+    TouchID.authenticate('To access your accounts', BIOMETRY_OPTIONS)
       .then(() => setPanelStatus(PANEL_STATUS.VERIFIED))
       .catch((error: any) => {
         setPanelStatus(PANEL_STATUS.PIN_ENTER);
--- app/components/VerifyPanel/verify-panel.options.ts
+++ app/components/VerifyPanel/verify-panel.options.ts
@@ -35,3 +35,15 @@ export const PANEL_STATUS: Record<VerifyPanelStatusKeys, VerifyPanelStatus> = {
   BIOMETRIC_VERIFY: 'biometric-verify',
   VERIFIED: 'verified',
 };
+
+export const BIOMETRY_OPTIONS = {
+  title: 'Authentication Required', // Android
+  imageColor: '#e00606', // Android
+  imageErrorColor: '#ff0000', // Android
+  sensorDescription: 'Touch sensor', // Android
+  sensorErrorDescription: 'Failed', // Android
+  cancelText: 'Cancel', // Android
+  fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
+  unifiedErrors: false, // use unified error messages (default false)
+  passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
+};
